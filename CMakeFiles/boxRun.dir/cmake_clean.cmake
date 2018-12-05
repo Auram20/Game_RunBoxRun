02024e97e97b4c66c30da25e370a593f7eb9a736
dragonbleapiece0 <shinkyoku.melody@gmail.com>
2018-12-05 12:20:48 +0100

    Init depot

---
+++ CMakeFiles/boxRun.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boxRun.dir/lib/src/main.cpp.o"
+  "bin/boxRun.pdb"
+  "bin/boxRun"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boxRun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lib/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+#include <SDL/SDL.h>
+#include <GL/glew.h>
+
+int main(void) {
+
+    std::cout << "Les IMAC sont des betes de code un truc du genre" << std::endl; //flush
+    
+    return 0;
+}
