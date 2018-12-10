8923278165688c78e33451a498b81100b9ede3a6
Auram20 <myriamanik97@gmail.com>
2018-12-10 16:47:37 +0100

    Doc almost fixed

---
+++ build/CMakeFiles/runBoxRun.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runBoxRun.dir/main.cpp.o"
+  "../runBoxRun.pdb"
+  "../runBoxRun"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runBoxRun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
