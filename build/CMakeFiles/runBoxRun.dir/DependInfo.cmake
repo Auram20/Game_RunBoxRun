0460fe990630068b54142bd1fcf0d60f21c1cae8
dragonbleapiece <nicolas.cusumano0@gmail.com>
2018-12-11 17:39:02 +0100

    correct cmake

--- build/CMakeFiles/runBoxRun.dir/DependInfo.cmake
+++ build/CMakeFiles/runBoxRun.dir/DependInfo.cmake
@@ -4,20 +4,20 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/6im2/manik/Game_RunBoxRun/main.cpp" "/home/6im2/manik/Game_RunBoxRun/build/CMakeFiles/runBoxRun.dir/main.cpp.o"
+  "/home/nicolas/Documents/IMAC/POO/Projet/Game_RunBoxRun/main.cpp" "/home/nicolas/Documents/IMAC/POO/Projet/Game_RunBoxRun/build/CMakeFiles/runBoxRun.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "/usr/include/SDL"
-  "../lib"
+  "../lib/include"
   "../game/include"
-  "../game/src"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nicolas/Documents/IMAC/POO/Projet/Game_RunBoxRun/build/game/CMakeFiles/game.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
