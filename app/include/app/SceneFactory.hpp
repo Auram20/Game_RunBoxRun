98161c7881b4bf243b49e4d7b56a507828f58077
dragonbleapiece <nicolas.cusumano0@gmail.com>
2018-12-19 15:59:33 +0100

    ??

---
+++ app/include/app/SceneFactory.hpp
@@ -0,0 +1,37 @@
+// ================================
+// OPENGL / C++ - IMAC 2
+// _________ RUNBOXRUN ___________ 
+// _________ SCENEFACTORY.HPP_____ 
+//================================
+
+#ifndef __SCENEFACTORY__HPP
+#define __SCENEFACTORY__HPP
+
+#include <glimac/glm.hpp>
+#include <GL/glew.h>
+#include <iostream>
+#include <vector>
+#include <utils/Error.hpp>
+#include <app/glm.hpp>
+
+namespace RUNBOXRUN
+{
+	/// \class SceneFactory
+	/// \brief class defining a new Scene from a Map.
+	class SceneFactory
+	{
+			
+		public:
+		// CONSTRUCTORS & DESTRUCTOR
+		SceneFactory(); /*!< default constructor */
+		~SceneFactory() = default; /*!< default destructor*/
+
+		private:
+		
+	};
+}
+
+#endif
+
+
+  
--- app/src/WindowEngine.cpp
+++ app/src/WindowEngine.cpp
@@ -41,9 +41,7 @@ int WindowEngine::initWindow(char* path)
 
 
     _MVMatrix = glm::translate(glm::mat4(1.f),glm::vec3(0.f, 0.f, -5.f));
-    _MVMatrix = glm::rotate(_MVMatrix, -95.f, glm::vec3(0.f, 1.f, 0.f));
-
-    _MVMatrix = glm::rotate(_MVMatrix, 15.f, glm::vec3(0.f, 1.f, 0.f));
+    _MVMatrix = glm::rotate(_MVMatrix, -45.f, glm::vec3(0.f, 1.f, 0.f));
 
     _NormalMatrix = glm::transpose(glm::inverse(_MVMatrix));
 
