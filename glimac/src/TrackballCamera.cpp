582669c7a323d8130768c4d7137f287e594d69f3
dragonbleapiece <nicolas.cusumano0@gmail.com>
2018-12-24 17:55:36 +0100

    corr

--- glimac/src/TrackballCamera.cpp
+++ glimac/src/TrackballCamera.cpp
@@ -20,7 +20,7 @@ void TrackballCamera::rotateUp(float degrees) {
     m_fAngleX += degrees;
 }
 
-glm::mat4 TrackballCamera::getViewMatrix() const {
+const glm::mat4 TrackballCamera::getViewMatrix() const {
     glm::mat4 viewMatrix =  glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, -m_fDistance));
     viewMatrix = glm::rotate(viewMatrix, glm::radians(m_fAngleY), glm::vec3(0.f, 1.f, 0.f));
     viewMatrix = glm::rotate(viewMatrix, glm::radians(m_fAngleX), glm::vec3(1.f, 0.f, 0.f));
