96104a12f227e53b3be373f82592d0289246769a
Auram20 <myriamanik97@gmail.com>
2019-01-02 15:04:47 +0100

    Added textured obj to check

--- app/src/Menu.cpp
+++ app/src/Menu.cpp
@@ -15,12 +15,12 @@ Menu::Menu()
 Scene Menu::mainMenuRender()
 {
 	glimac::Texture BG(FilePath("../assets/img/BG.jpg"),aiTextureType_NONE);
-
+	glimac::Model monBG(FilePath("../assets/obj/BG.obj"));
 	glimac::Box Rectangle1(1,1,0);
 	Scene scene;
 	Object *Rec = new Object(1, glm::vec3(0,0,-5), glm::vec3(100), glm::vec3(100));
 
-	GameObject *go = new GameObject(Rectangle1, *Rec,Transform(glm::vec3(0, 0, -5),glm::vec3(20)));
+	GameObject *go = new GameObject(monBG, *Rec,Transform(glm::vec3(0, 0, -5),glm::vec3(0.5),glm::vec3(0,0,0)));
 	scene.push(go);
 
 	return scene;
