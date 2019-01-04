3ca8df49db06942b84aa717076564577323b0e48
Auram20 <myriamanik97@gmail.com>
2019-01-04 15:06:50 +0100

    Added frame

--- app/src/Scene.cpp
+++ app/src/Scene.cpp
@@ -19,14 +19,19 @@ void Scene::mainMenu()
 {	
 
     glDisable(GL_DEPTH_TEST);
-	//glimac::Texture BG(glimac::FilePath("../assets/img/BG.jpg"),aiTextureType_DIFFUSE);
 	glimac::Model monBG(glimac::FilePath("../assets/obj/MenuBG.obj"));
+	glimac::Model myCorner(glimac::FilePath("../assets/obj/CornerBox.obj"));
+
 	glimac::Box Rectangle1(1,1,0);
 	Scene scene;
 	Object *Rec = new Object(1, glm::vec3(0,0,-5), glm::vec3(1), glm::vec3(100));
 
-	GameObject *go = new GameObject(monBG, *Rec,Transform(glm::vec3(-0.5, -0.8, -5),glm::vec3(0.125)));
-	this->push(go);
+	GameObject *bg = new GameObject(monBG, *Rec,Transform(glm::vec3(-0.5, -0.8, -5),glm::vec3(0.125)));
+
+	GameObject *corner = new GameObject(myCorner, *Rec,Transform(glm::vec3(-2.6, -2.3, -4.9),glm::vec3(0.80,0.45,0)));
+
+	this->push(bg);
+	this->push(corner);
 	
     glEnable(GL_DEPTH_TEST);
 }
